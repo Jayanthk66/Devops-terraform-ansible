314c63c6b9918c96a396d0977d0f0bb948cb3fe6
Jayanthk66 <132988048+Jayanthk66@users.noreply.github.com>
2023-08-25 12:37:12 +0530

    Update server.tf
    AMI update

--- server.tf
+++ server.tf
@@ -11,14 +11,14 @@ provider "aws" {
 region = "us-west-1"
 }
 resource "aws_instance" "myawsserver" {
-  ami = "ami-09c72d5a7ce529ad5"
+  ami = "ami-0911e88fb4687e06b"
   instance_type = "t2.micro"
   key_name = "jayanth-import"
 
   tags = {
-    Name = "Raman-DevOps-batch-server"
+    Name = "Jayanth-DevOps-batch-server"
     env = "Production"
-    owner = "Raman Khanna"
+    owner = "Jayanth Krishnappa"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /tmp/inv"
